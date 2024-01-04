97987a686c33ef36b532a957ca4fa2fbe4db621e
Nilvalox <128976944+Nilvalox@users.noreply.github.com>
2024-01-04 19:00:22 +0000

    updated

--- k-string.cpp
+++ k-string.cpp
@@ -1,20 +1,45 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main() {
+int main()
+{
+    int k;
+    string s;
+    cin >> k >> s;
 
-  long t;
-  cin >> t;
+    int count[26] = {0};
+    for (size_t i = 0; i < s.length(); ++i)
+    {
+        count[s[i]-'a'] += 1;
+    }
 
-  while (t--) {
-    char 
-    cin >> k;
-    if (k <= n) { cout << (k + n - 1)
+    string s1;
+    size_t c = 0;
+    for (; c < 26; ++c)
+    {
+        if (count[c] % k != 0)
+        {
+            break;
+        }
+        else
+        {
+            s1.append(count[c] / k, 'a' + c);
+        }
+    }
+    if (c == 26)
+    {
+        while (k--)
+        {
+            cout << s1;
+        }
+        cout << endl;
+    }
+    else
+    {
+        cout << -1 << endl;
+    }
 
-      
-  }
-  else cout << "-1";
-
-  return 0;
+    return 0;
 }
