a46767cc46a516709e5d784938ad57750fe921bc
NilotpalMaitra <128976944+NilotpalMaitra@users.noreply.github.com>
2024-01-05 16:50:55 +0000

    updated

---
+++ buttons.cpp
@@ -0,0 +1,20 @@
+/*
+n = 4
+    _ _ _ _
+
+    1*(n-1) + right wrong 2*(n-2) + right right wrong 3*(n-3) ..... n(n-n)
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int sum = n;
+    for (int i = 1; i<n;i++) sum+=(n-i)*i;
+    cout << sum;
+
+}
